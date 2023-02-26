4c1294560fd3e4d6000d25f6cb50a849b682f5ec
Anubhav Kumar <119742705+anubhavkumar503@users.noreply.github.com>
2023-02-26 11:57:31 +0530

    Create ATMOperationinterface.java

---
+++ ATMOperationinterface.java
@@ -0,0 +1,7 @@
+public interface ATMOperationinterface {
+        public void viewbalance();
+       public void withdrawAmount(double withdrawamount);
+       public void DepositAmount(double depositamount);
+       public void viewministatement();
+      
+}
